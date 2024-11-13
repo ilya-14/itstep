9ae245cb0e42724c843e1bb7ee519226653c0d06
USER <protsilly1@gmail.com>
2024-11-13 15:16:51 +0200

    dz-6

---
+++ dz-6.py
@@ -0,0 +1,7 @@
+num1 = float(input("Введіть перше число: "))
+num2 = float(input("Введіть друге число: "))
+num3 = float(input("Введіть третє число: "))
+
+result = num1 * num2 * num3
+
+print(f"Результат множення: {result}")
